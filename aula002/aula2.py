7894a96ff7f875fb4b74ae4eff73f935cbd0e694
ArthurDamasio <arthurdamasio@live.com>
2021-08-28 20:09:17 -0300

    primeiro commit

---
+++ aula002/aula2.py
@@ -0,0 +1,11 @@
+'''
+#print(123456)
+#print('Luiza', 'Damasio', '3coisa')
+
+print('Arthur', 'Damasio', sep='-', end='####')
+print('Arthur', 'e', 'Damasio', sep='-', end='')
+368.318.688-78
+'''
+
+print(368,318,688, sep='.',end='-')
+print(78)
