ed39b00af13cef2d37fb2dc0ca99d09a31536b92
ArthurDamasio <arthurdamasio@live.com>
2021-08-29 20:39:04 -0300

    Aula 24 e 23 atualizada

--- aula024/aula024.py
+++ aula024/aula024.py
@@ -1,3 +1,18 @@
-'''
+"""
 
-'''
+Emoticon
+print("\U0001F917")
+print("\U0001F918")
+print("\U0001F919")
+print("\U0001F920")
+
+For/ Else em Python
+"""
+
+var = ['Arthur Damasio', 'Joaozinho', 'Maria']
+
+for valor in var:
+    if valor.startswith('J'):
+        print('Começa com J')
+    else:
+        print('Não começa com J')
